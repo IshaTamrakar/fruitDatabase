ada4edbb9697903bc7d147991c5f9329ae0d6a96
IshaTamrakar <61106634+IshaTamrakar@users.noreply.github.com>
2023-03-21 21:09:04 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -29,10 +29,19 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/fruitDB');
   const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,              //Adding Validations
+        max: 10
+    },
     reviews: String
 })
+
+console.log("Server Running");
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
@@ -42,11 +51,18 @@ const fruit = new Fruit({
      reviews: "Tasty"
 })
 
-// fruit.save();
+const peach = new Fruit({
+    name: "Peach",
+    rating: 7,
+     reviews: "Tasty"
+})
+
+ peach.save();
 
 const personSchema = new mongoose.Schema({
     name : String,
-    age : Number
+    age : Number,
+    favFruit : fruitSchema
 })
 
 const Person = mongoose.model("Person" , personSchema);
@@ -56,27 +72,45 @@ const person = new Person({
     age: 27
 })
 
-// person.save()
-
-const banana = new Fruit({
-    name: "Banana",
-    rating: 5,
-    reviews: "vhsdbhdvv"
-});
-
-const kiwi = new Fruit({
-    name: "Kiwi",
-    rating: 8,
-    reviews: "dshvjc"
-});
-
-Fruit.insertMany([kiwi,banana]).then(function(){
-    console.log("data inserted");
-}).catch(function(error){
-    console.log(error);
+const newPerson = new Person({
+    name: "Amy",
+    age: 12,
+    favFruit: peach
 })
 
+newPerson.save();
+
+// person.save()
+
+// const banana = new Fruit({
+//     name: "Banana",
+//     rating: 5,
+//     reviews: "vhsdbhdvv"
+// });
+
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     rating: 8,
+//     reviews: "dshvjc"
+// });
+
+// Fruit.insertMany([kiwi,banana]).then(function(){
+//     console.log("data inserted");
+// }).catch(function(error){
+//     console.log(error);
+// })
+
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+// Fruit.find(fruit) 
+// console.log(Fruit.find(fruit) );
+
+
+
+
+
+
+
 }
 
 
